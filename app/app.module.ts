758980b62a4f0344ed0f5cf825294aa268b7f1cf
Alfonso Aranzazu <aaranzaz@uci.edu>
2017-03-28 21:35:50 -0700

    simplifying dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent }   from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
