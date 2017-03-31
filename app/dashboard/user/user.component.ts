25abee7d7ffd84970e52fe12b070252ae649489a
Alfonso Aranzazu <aaranzaz@uci.edu>
2017-03-31 04:54:48 -0700

    adds notifications for creating employees

--- app/dashboard/user/user.component.ts
+++ app/dashboard/user/user.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {EmployeeService} from "../../services/employee-service";
+import {NotificationService} from "../../services/notifications-service";
+declare var $: any;
 
 @Component({
     selector: 'user-cmp',
@@ -7,24 +9,26 @@ import {EmployeeService} from "../../services/employee-service";
     templateUrl: 'user.component.html'
 })
 
-export class UserComponent implements OnInit{
+export class UserComponent {
     
-    constructor(private employeeService: EmployeeService) {
+    constructor(private employeeService: EmployeeService, private notification: NotificationService) {
         
-    }
-    
-    ngOnInit(){
-        // $.getScript('../../../assets/js/material-dashboard.js');
-
     }
 
     public createEmployee(name, phone, supervisor) {
         this.employeeService.createEmployee(name, phone, supervisor)
-            .then((res) => {
-                console.log('create employee response: ', res);
+            .then((response) => {
+                if (response.success) {
+                    this.notification.presentSuccessNotification("Employee created");
+                    // make dashboard link on sidebar active
+                    $("body > my-app > div > div.sidebar > sidebar-cmp > div.sidebar-wrapper > div > ul > li:nth-child(1) > a > p").trigger("click");
+                } else {
+                    this.notification.presentErrorNotification("Employee already exists");
+                }
+                
             })
-            .catch((err) => {
-                console.log('create employee err: ', err);
+            .catch(() => {
+                this.notification.presentErrorNotification("There was a problem creating the employee");
             });
     }
 }
